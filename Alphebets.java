f23e00cbd3ea1c197dfbf93e8bbc2837dd862d29
Bhadri <97180245+Bhadraraj@users.noreply.github.com>
2022-12-26 01:11:52 +0530

    Add files via upload

---
+++ Alphebets.java
@@ -0,0 +1,9 @@
+package JavaPrograms;
+
+class Alphabets {
+    public static void main(String args[]) {
+        char ch;
+        for (ch = 'a'; ch <= 'z'; ch++)
+            System.out.println(ch);
+    }
+}
---
+++ SwapNumbers.java
@@ -0,0 +1,16 @@
+package JavaPrograms;
+import java.util.Scanner;
+class SwapNumbers {
+    public static void main(String args[]) {
+        int x, y, temp;
+        System.out.println("Enter x and y");
+        Scanner sc = new Scanner(System.in);
+        x = sc.nextInt();
+        y = sc.nextInt();
+        System.out.println("Before Swapping\nx = " + x + "\ny = " + y);
+        temp = x;
+        x = y;
+        y = temp;
+        System.out.println("After Swapping\nx = " + x + "\ny = " + y);
+    }
+}
